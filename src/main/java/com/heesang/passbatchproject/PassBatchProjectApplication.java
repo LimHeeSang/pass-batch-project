a7101d0a5bc5cd553da535cf2736e282b150e712
heesang <dlagmltkd12@naver.com>
2023-02-12 02:49:04 +0900

    initial commit

---
+++ src/main/java/com/heesang/passbatchproject/PassBatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.heesang.passbatchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassBatchProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heesang/passbatchproject/PassBatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heesang.passbatchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
